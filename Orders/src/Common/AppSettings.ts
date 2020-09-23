a28069c47d426c83caeffae9f0939bc3cc10ebae
V-Kish <vasja.kish@mail.ru>
2020-09-23 16:46:00 +0300

    add fetch to firebase token send to server

--- Orders/src/Common/AppSettings.ts
+++ Orders/src/Common/AppSettings.ts
@@ -2,10 +2,6 @@ class AppSettings {
   static get endpoint() {
     return 'https://api-bonusclub.getmyfit.online/';
   }
-
-  static get fileEndpoint() {
-    return 'https://testapi-files.getmyfit.online';
-  }
 }
 
 export {AppSettings};
