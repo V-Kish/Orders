e8164aae6c65a1160ef81b3da4542f85ebdbf6f0
V-Kish <vasja.kish@mail.ru>
2020-09-24 23:18:22 +0300

    add states

--- Orders/src/View/HomeView/HomeListView.tsx
+++ Orders/src/View/HomeView/HomeListView.tsx
@@ -1,26 +1,29 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View, Text, Image} from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import { useSelector } from 'react-redux';
-import { OrderItem } from './OrderItem';
-
+import {ScrollView} from 'react-native-gesture-handler';
+import {useSelector} from 'react-redux';
+import {OrderItem} from './OrderItem';
+import {
+  mockupHeightToDP as hp,
+  mockupWidthToDP as wp,
+} from '../../constants/Dimensions';
 export const HomeListView = () => {
-    const orders = useSelector((state: reduxTypes) => state.dictionaries.orders);
-    console.log('orders', orders)
+  const orders = useSelector((state: reduxTypes) => state.dictionaries.orders);
+  console.log('orders', orders);
   return (
-
     <ScrollView>
-        <View style={styles.container}>
-            {orders.Items && orders.Items.map((item: any)=>{
-                return <OrderItem key={item.system.orderNum} item={item}/>
-            })}
-        </View>
+      <View style={styles.container}>
+        {orders.Items &&
+          orders.Items.map((item: any) => {
+            return <OrderItem key={item.system.orderNum} item={item} />;
+          })}
+      </View>
     </ScrollView>
   );
 };
 
-
 const styles = StyleSheet.create({
-    container: {
-    }
-})
+  container: {
+    paddingBottom: hp(80),
+  },
+});
