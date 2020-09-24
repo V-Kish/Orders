54101f1e728e8b92feac276444fa6e7b214afe40
V-Kish <vasja.kish@mail.ru>
2020-09-24 21:55:15 +0300

    add

--- Orders/src/View/HomeView/HomeView.tsx
+++ Orders/src/View/HomeView/HomeView.tsx
@@ -1,48 +1,54 @@
-import React, { useRef, useState } from 'react';
-import {StyleSheet, View, Text, Image, Dimensions, Animated} from 'react-native';
-import { HomeListView } from './HomeListView';
-import { SearchContainer } from './SearchContainer';
+import React, {useRef, useState} from 'react';
 import {
-    mockupHeightToDP as hp,
-    mockupWidthToDP as wp,
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Dimensions,
+  Animated,
+} from 'react-native';
+import {HomeListView} from './HomeListView';
+import {SearchContainer} from './SearchContainer';
+import {
+  mockupHeightToDP as hp,
+  mockupWidthToDP as wp,
 } from '../../constants/Dimensions';
-import { DropDownSelector } from './DropDownSelector';
+import {DropDownSelector} from './DropDownSelector';
 
 export const HomeView = () => {
-  const [dropdown, setDropdown] = useState(false)
-  
+  const [dropdown, setDropdown] = useState(false);
+
   const switchDropDown = () => {
-      setDropdown(!dropdown)
-  }
+    setDropdown(!dropdown);
+  };
   return (
     <View style={styles.container}>
-        <SearchContainer changeDropDownVisible={switchDropDown}/>
-        <DropDownSelector dropdown={dropdown}/>
-        <HomeListView/>
+      <SearchContainer changeDropDownVisible={switchDropDown} />
+      <DropDownSelector dropdown={dropdown} />
+      <HomeListView />
     </View>
   );
 };
 
-
 const styles = StyleSheet.create({
-    container: {
-        // flex: 1,
-    },
-    dropDown: {
-        position: 'absolute',
-        top: hp(100),
-        right:0,
-        left: 0,
-        backgroundColor: 'rgba(0,0,0,.3)',
-        overflow: 'hidden',
-        // height: 200,
-        zIndex: 999999
-    },
-    dropDownShowed: {
-        height: Dimensions.get('screen').height
-    },
-    content: {
-        backgroundColor: 'red',
-        minHeight: Dimensions.get('screen').height
-    }
-})
+  container: {
+    // flex: 1,
+  },
+  dropDown: {
+    position: 'absolute',
+    top: hp(100),
+    right: 0,
+    left: 0,
+    backgroundColor: 'rgba(0,0,0,.3)',
+    overflow: 'hidden',
+    // height: 200,
+    zIndex: 999999,
+  },
+  dropDownShowed: {
+    height: Dimensions.get('screen').height,
+  },
+  content: {
+    backgroundColor: 'red',
+    minHeight: Dimensions.get('screen').height,
+  },
+});
