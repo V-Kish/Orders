c092304f9ff095c30c464139c53ccb2f629ce7b4
V-Kish <vasja.kish@mail.ru>
2020-09-24 14:25:43 +0300

    add Dictionaries.ts

--- Orders/src/View/HeaderView/HeaderView.tsx
+++ Orders/src/View/HeaderView/HeaderView.tsx
@@ -1,60 +1,53 @@
-import React, {useState} from 'react';
-import {StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native';
-import { COLORS } from '../../constants/colors';
+import React from 'react';
+import {StyleSheet, View, Text, Image} from 'react-native';
+import {COLORS} from '../../constants/colors';
 import {
-    mockupHeightToDP as hp,
-    mockupWidthToDP as wp,
-  } from '../../constants/Dimensions';
+  mockupHeightToDP as hp,
+  mockupWidthToDP as wp,
+} from '../../constants/Dimensions';
 
 export const HeaderView = ({icon, title, desc, color = COLORS.HEADER_BLUE}) => {
   return (
     <View style={{...styles.container, backgroundColor: color}}>
-        <View style={styles.imageView}>
-            <Image
-                source={icon}
-                style={styles.image}
-            />
-        </View>
-        <View style={styles.textContainer}>
-            <Text style={styles.title}>{title}</Text>
-            {desc !== undefined && <Text style={styles.desc}>{desc}</Text>}
-        </View>
+      <View style={styles.imageView}>
+        <Image source={icon} style={styles.image} />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{title}</Text>
+        {desc !== undefined && <Text style={styles.desc}>{desc}</Text>}
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        height: hp(90),
-        paddingHorizontal: wp(20),
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 6,
-        },
-        shadowOpacity: 0.37,
-        shadowRadius: 7.49,
-        
-        elevation: 12,
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: hp(90),
+    paddingHorizontal: wp(20),
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 6,
     },
-    imageView: {
-        justifyContent: 'center'
-    },
-    image: {
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
 
-    },
-    textContainer: {
-        paddingHorizontal: wp(10),
-        justifyContent: 'center'
-    },
-    title: {
-        fontSize: 20,
-        color: 'white',
-        fontWeight: 'bold'
-    },
-    desc: {
-
-    },
+    elevation: 12,
+  },
+  imageView: {
+    justifyContent: 'center',
+  },
+  image: {},
+  textContainer: {
+    paddingHorizontal: wp(10),
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  desc: {},
 });
--- Orders/src/screens/RegistrationScreen.tsx
+++ Orders/src/screens/RegistrationScreen.tsx
@@ -1,30 +1,27 @@
 import React from 'react';
 import {StyleSheet, View, ScrollView} from 'react-native';
-import { RegistrationForm } from '../View/RegistrationView/RegistrationForm';
-import { RegistrationLogo } from '../View/RegistrationView/RegistrationLogo';
-import { HeaderView } from '../View/HeaderView/HeaderView';
-import { ICONS } from '../constants/icons';
+import {RegistrationForm} from '../View/RegistrationView/RegistrationForm';
+import {RegistrationLogo} from '../View/RegistrationView/RegistrationLogo';
+
+import {ICONS} from '../constants/icons';
+import {HeaderView} from '../View/HeaderView/HeaderView';
 
 export const RegistrationScreen = () => {
   return (
-      <>
-        <HeaderView
-            icon={ICONS.headerPeoples}
-            title="Вхід до системи"
-          />
-        <ScrollView>
+    <>
+      <HeaderView icon={ICONS.headerPeoples} title="Вхід до системи" />
+      <ScrollView>
         <View style={styles.container}>
-          <RegistrationLogo/>
-          <RegistrationForm/>
+          <RegistrationLogo />
+          <RegistrationForm />
         </View>
       </ScrollView>
     </>
   );
 };
 
-
 const styles = StyleSheet.create({
   container: {
-    padding: 20
-  }
-})
+    padding: 20,
+  },
+});
