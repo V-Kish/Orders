086a8f35a06d0bca494059b82bfa8ff916e233b9
V-Kish <vasja.kish@mail.ru>
2020-09-25 09:56:47 +0300

    fix orders

--- Orders/src/helpers/StatusToType.ts
+++ Orders/src/helpers/StatusToType.ts
@@ -1,6 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-const types = ['new', 'accept', 'wait', 'reject', 'done']
-export const statusToType = (status:number) => {
-    return types[status-1]
+const types = ['new', 'accept', 'wait', 'reject', 'done'];
+export const statusToType = (status: number) => {
+  return types[status - 1];
+};
+export function recalculateSumResult(item) {
+    try {
+        if (itemHistory.detail.rateCurrency.buy < itemHistory.detail.rateCurrencyTo.buy) {
+            return (itemHistory.detail.sum / itemHistory.detail.rate).toFixed(2);
+        } else {
+            return (itemHistory.detail.sum * itemHistory.detail.rate).toFixed(2);
+        }
+    } catch (e) {
+        this.preloaderHistory.isShowPreloader = false;
+    }
 }
